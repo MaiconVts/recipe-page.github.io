0840ce512bf6ed2186ff269b2546c88effe9c174
maicon-theodoro <maicon.vitorsilva@hotmail.com>
2025-03-27 02:46:39 -0300

    Initial commit

---
+++ recipe-page-main/novo.cs
@@ -0,0 +1,22 @@
+// int = inteiro
+// double = real
+// float = real
+// string = caractere
+// bool = booleano
+
+using System; 
+
+class MainClass {
+    public static void Main (string[] args) {
+        int idade;
+        double altura, peso, copiaPeso, IMC;
+        string nome;
+        bool temCNH;
+
+        idade = 23;
+        altura = 1.76;
+        peso = 78.9;
+        nome = "João da Silva";
+        temCNH = true; 
+    }
+}
